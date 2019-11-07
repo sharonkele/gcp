165a6d887dae1e5b37d95a66d5798680cd273f71
sharonkele <55387354+sharonkele@users.noreply.github.com>
2019-11-07 10:57:38 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@ curl https://get.docker.com | sudo bash
 sudo usermod -aG docker $(whoami)
 sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-git clone https://github.com/sharonkele/gcp -branch development 
+git clone https://github.com/sharonkele/gcp 
 cd gcp
+git checkout development 
 sudo docker-compose up -d --build
